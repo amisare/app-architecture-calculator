aa6f11317f784242ed80980860356fd18e6941e7
HaiJun Gu <243297288@qq.com>
2019-12-15 21:56:22 +0800

    modify: 第三方库引用结构修改

--- Local Podspecs/ComponentKit.podspec
+++ Local Podspecs/ComponentKit.podspec
@@ -11,7 +11,7 @@ Pod::Spec.new do |s|
   s.social_media_url = 'https://twitter.com/componentkit'
   s.platform = :ios, '7.0'
   s.requires_arc = true
-  s.vendored_frameworks = 'ComponentKit.framework'
+  s.vendored_frameworks = 'Carthage/Build/iOS/ComponentKit.framework'
   s.frameworks = 'UIKit', 'CoreText'
   s.library = 'c++'
   s.xcconfig = {
--- Pods/Target Support Files/Pods-CalculatorMVC/Pods-CalculatorMVC-frameworks.sh
+++ Pods/Target Support Files/Pods-CalculatorMVC/Pods-CalculatorMVC-frameworks.sh
@@ -161,13 +161,19 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../Local Podspecs/ComponentKit.framework"
+  install_framework "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/ComponentKit.framework"
+  install_dsym "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/ComponentKit.framework.dSYM"
+  install_bcsymbolmap "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/B148C858-5768-3317-B03D-44B83A2D2315.bcsymbolmap"
+  install_bcsymbolmap "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/5606CAF9-BBD7-3F8F-914C-A042FBD4F289.bcsymbolmap"
   install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../Local Podspecs/ComponentKit.framework"
+  install_framework "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/ComponentKit.framework"
+  install_dsym "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/ComponentKit.framework.dSYM"
+  install_bcsymbolmap "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/B148C858-5768-3317-B03D-44B83A2D2315.bcsymbolmap"
+  install_bcsymbolmap "${PODS_ROOT}/../Local Podspecs/Carthage/Build/iOS/5606CAF9-BBD7-3F8F-914C-A042FBD4F289.bcsymbolmap"
   install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
